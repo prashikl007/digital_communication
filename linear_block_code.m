8b1dd01a3a156d9acf5a20fea015c7995a360524
Prashik Lokhande <50161593+prashikl007@users.noreply.github.com>
2025-10-16 12:02:31 +0530

    Update linear_block_code.m

--- linear_block_code.m
+++ linear_block_code.m
@@ -1,78 +1,44 @@
-% Given H Matrix 
-H = [1 0 1 1 1 0 0; 
-     1 1 0 1 0 1 0; 
-     0 1 1 1 0 0 1] 
-  
-k = 4; 
-n = 7; 
-  
-% Generating G Matrix 
-  
-% Taking the H Matrix Transpose 
-P = H';   
-  
-% Making a copy of H Transpose Matrix 
-L = P;   
-  
-% Taking the last 4 rows of L and storing 
-L((5:7), : ) = [];   
-  
-% Creating a Identity matrix of size K x K 
-I = eye(k);   
-  
-% Making a 4 x 7 Matrix 
-G = [I L]   
-  
-% Generate U data vector, denoting all information sequences 
-no = 2 ^ k 
-  
-% Iterate through an Unit-Spaced Vector 
-for i = 1 : 2^k   
-  
-  % Iterate through Vector with Specified Increment  
-  % or in simple words here we are decrementing 4 till we get 1     
-  for j = k : -1 : 1  
-    if rem(i - 1, 2 ^ (-j + k + 1)) >= 2 ^ (-j + k) 
-      u(i, j) = 1; 
-    else
-      u(i, j) = 0; 
-    end
-      
-    % To avoid displaying each iteration/loop value 
-    echo off;  
-  end
-end
-  
-echo on; 
-u 
-  
-% Generate CodeWords 
-c = rem(u * G, 2) 
-  
-% Find the min distance 
-w_min = min(sum((c(2 : 2^k, :))')) 
-  
-% Given Received codeword 
-r = [0 0 0 1 0 0 0]; 
-r 
-  
-p = [G(:, n - k + 2 : n)]; 
-  
-%Find Syndrome 
-ht = transpose(H) 
-  
-s = rem(r * ht, 2) 
-  
-for i = 1 : 1 : size(ht) 
-  if(ht(i,1:3)==s) 
-    r(i) = 1-r(i); 
-    break; 
-  end
-end
-  
-disp('The Error is in bit:') 
-disp(i) 
-  
-disp('The Corrected Codeword is :') 
-disp(r)
+% Minimal Hamming (7,4)
 
+G = [1 0 0 0 1 1 0;
+     0 1 0 0 1 0 1;
+     0 0 1 0 0 1 1;
+     0 0 0 1 1 1 1];
+
+P = G(:,5:7);
+H = [P' eye(3)];
+
+% Example message & codeword
+msg = [1 0 1 1];
+code = mod(msg * G, 2);
+
+% Received word (introduce single-bit error)
+r = code;
+r(3) = mod(r(3)+1,2);
+
+% Syndrome
+s = mod(H * r', 2);
+
+fprintf('Received word: '); disp(r);
+fprintf('Syndrome: '); disp(s');
+
+% Find error position by matching syndrome to columns of H
+err_pos = 0;
+for j = 1:size(H,2)
+    if isequal(H(:,j), s)
+        err_pos = j;
+        break;
+    end
+end
+
+if err_pos == 0
+    if all(s==0)
+        disp('No error detected.');
+    else
+        disp('Syndrome nonzero but no single-bit match found (multi-bit or unknown).');
+    end
+else
+    fprintf('Error detected at bit position: %d\n', err_pos);
+    r(err_pos) = mod(r(err_pos)+1,2);    % correct
+    fprintf('Corrected codeword: '); disp(r);
+end
