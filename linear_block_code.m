aad158722dc0836fd677a5d8e7c8cc8aaa728c83
Prashik Lokhande <50161593+prashikl007@users.noreply.github.com>
2025-10-16 12:39:20 +0530

    Update linear_block_code.m

--- linear_block_code.m
+++ linear_block_code.m
@@ -1,4 +1,4 @@
-% Minimal Hamming (7,4)
+% Linear Block Code (7,4) with Error Detection and Correction
 
 G = [1 0 0 0 1 1 0;
      0 1 0 0 1 0 1;
@@ -8,37 +8,38 @@ G = [1 0 0 0 1 1 0;
 P = G(:,5:7);
 H = [P' eye(3)];
 
-% Example message & codeword
+% Example message and received word (1-bit error introduced)
 msg = [1 0 1 1];
 code = mod(msg * G, 2);
-
-% Received word (introduce single-bit error)
-r = code;
-r(3) = mod(r(3)+1,2);
-
-% Syndrome
-s = mod(H * r', 2);
+r = code; 
+r(3) = mod(r(3)+1,2);   % introduce error
 
 fprintf('Received word: '); disp(r);
+
+% Calculate Syndrome
+s = mod(H * r', 2);
 fprintf('Syndrome: '); disp(s');
 
-% Find error position by matching syndrome to columns of H
-err_pos = 0;
-for j = 1:size(H,2)
-    if isequal(H(:,j), s)
-        err_pos = j;
-        break;
+% Check for errors
+if all(s == 0)
+    disp('No error detected.');
+else
+    % Find error bit position
+    for j = 1:size(H,2)
+        if isequal(H(:,j), s)
+            err_pos = j;
+            break;
+        end
     end
+    fprintf('Error detected at bit position: %d\n', err_pos);
+
+    % Display error vector
+    e = zeros(1, size(H,2));
+    e(err_pos) = 1;
+    fprintf('Error vector: '); disp(e);
+
+    % Correct codeword
+    r_corr = mod(r + e, 2);
+    fprintf('Corrected codeword: '); disp(r_corr);
 end
 
-if err_pos == 0
-    if all(s==0)
-        disp('No error detected.');
-    else
-        disp('Syndrome nonzero but no single-bit match found (multi-bit or unknown).');
-    end
-else
-    fprintf('Error detected at bit position: %d\n', err_pos);
-    r(err_pos) = mod(r(err_pos)+1,2);    % correct
-    fprintf('Corrected codeword: '); disp(r);
-end
